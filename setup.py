08e4571722191894cc1e08c8504c6dff2100d45a
hoge <hoge@hoge.com>
2022-07-01 15:27:05 +0900

    modify readme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'cfn-docgen',
-    version = "0.0.5",
+    version = "0.0.6",
     author = 'Takehiro Horie',
     author_email = 'horie.takehiro@outlook.jp',
     license = 'MIT License',
