cfb9490288eaa37d9f1cb1a74c3e214f8b4769ed
hoge <hoge@hoge.com>
2022-10-10 14:31:41 +0900

    create pipeline

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'cfn-docgen',
-    version = "0.1.0",
+    version = "0.1.1",
     author = 'Takehiro Horie',
     author_email = 'horie.takehiro@outlook.jp',
     license = 'MIT License',
