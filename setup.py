9fc637bdad2fc8db5ccc4ff173925faefdafc25b
hoge <hoge@hoge.com>
2022-11-04 15:28:53 +0900

    v0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'cfn-docgen',
-    version = "0.1.3",
+    version = "0.1.4",
     author = 'Takehiro Horie',
     author_email = 'horie.takehiro@outlook.jp',
     license = 'MIT License',
