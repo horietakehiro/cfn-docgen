9f012fde7c0c9ebef3f8c9c50626ef44f83b6843
horietakehiro <nashhoward1312@docomo.ne.jp>
2023-07-25 16:25:00 +0900

    add files

---
+++ src/domain/ports/cfn_template_provider.py
@@ -0,0 +1,8 @@
+from abc import ABC
+
+class CfnTemplateProvider(ABC):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    
---
+++ src/domain/services/cfn_docgen_service.py
@@ -0,0 +1,5 @@
+
+class CfnDocgenService(object):
+
+    def __init__(self, cfn_specification_repository) -> None:
+        pass
