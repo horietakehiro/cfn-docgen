9161202e85c81d4652bd283c239bc07f95575714
horietakehiro <horie.takehiro@outlook.jp>
2023-09-08 18:27:05 +0900

    increment version

--- src/cfn_docgen/__init__.py
+++ src/cfn_docgen/__init__.py
@@ -8,7 +8,7 @@ from cfn_docgen.domain.services.cfn_docgen_service import (
 )
 
 
-__version__="0.9.1"
+__version__="0.9.2"
 
 CfnDocgenService = __CfnDocgenService
 CfnDocgenServiceCommandInput = __CfnDocgenServiceCommandInput
